3af9383ddb6adf07a6d6b5bfa9f7000cbe7c0b5b
Dante <dalvarezt@unjbg.edu.pe>
2024-07-09 21:55:15 -0500

    Mejora al ejercicio 1 de registros: Se mejoro la funcion de agregar contacto

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -12,69 +12,71 @@ struct contactoEmail {
 	string nacionalidad;
 };
 
-//FUNCION AGREGAR UN CONTACTO
-void agregarContacto( int cantidadContactos ) {
-	contactoEmail contact[cantidadContactos];
-	
-	for( int i = 0; i < cantidadContactos; i++ ) {
-		cout << "// AGREGAR UN CONTACTO Nro: " << i + 1 << " //" << endl;
-		cout << "Ingrese nombres completos: ";
-		cin.ignore();
-		getline(cin, contact[i].nombresCompletos);
-		
-		cout << "Ingrese el genero: ";
-		cin >> contact[i].sexo;
-			
-		cout << "Ingrese la edad: ";
-		cin >> contact[i].edad;
-		
-		cout << "Ingrese el telefono: ";
-		cin >> contact[i].telefono;
-			
-		cout << "Ingrese el email: ";
-		cin >> contact[i].email;
-			
-		cout << "Ingrese la nacionalidad: ";
-		cin >> contact[i].nacionalidad;
-	}
+// FUNCIÓN AGREGAR UN CONTACTO
+void agregarContacto(contactoEmail contactos[], int &contadorContactos) {
+    if (contadorContactos < 10) {
+        cout << "// AGREGAR UN CONTACTO Nro: " << contadorContactos + 1 << " //" << endl;
+        cin.ignore();
+        cout << "Ingrese nombres completos: ";
+        getline(cin, contactos[contadorContactos].nombresCompletos);
+
+        cout << "Ingrese el genero: ";
+        getline(cin, contactos[contadorContactos].sexo);
+
+        cout << "Ingrese la edad: ";
+        cin >> contactos[contadorContactos].edad;
+        cin.ignore();
+
+        cout << "Ingrese el telefono: ";
+        getline(cin, contactos[contadorContactos].telefono);
+
+        cout << "Ingrese el email: ";
+        getline(cin, contactos[contadorContactos].email);
+
+        cout << "Ingrese la nacionalidad: ";
+        getline(cin, contactos[contadorContactos].nacionalidad);
+
+        contadorContactos++;
+    } else {
+        cout << "No se pueden agregar más contactos. Límite alcanzado." << endl;
+    }
 }
 
-//FUNCION SWITCH PARA EL MENU DE OPCIONES
-void programa( int decision, bool &bucle ) {
-	int cantidadContactos;
-	switch ( decision ) {
-		case 1:
-			cout << "Ingrese cuantos contactos desea agregar: ";
-			cin >> cantidadContactos;
-			agregarContacto( cantidadContactos );	
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			bucle = true;
-			break;
-		default:
-			cout << "Ingreso un numero invalido" << endl;
-			break;
-	}
+// FUNCIÓN SWITCH PARA EL MENÚ DE OPCIONES
+void programa(int decision, bool &bucle, contactoEmail contactos[], int &contadorContactos) {
+    switch (decision) {
+        case 1:
+            agregarContacto(contactos, contadorContactos);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            bucle = true;
+            break;
+        default:
+            cout << "Ingreso un numero invalido" << endl;
+            break;
+    }
 }
 
 int main() {
 	int decision;
-	bool bucle = false;
-	
-	do {
-		cout << "1) Agregar un contacto" << endl;
-		cout << "2) Eliminar un contacto" << endl;
-		cout << "3) Mostrar contactos registrados" << endl;
-		cout << "4) Mostrar contactos existentes, ordenado por servidor de correo" << endl;
-		cout << "5) Salir del programa" << endl;
-		cin >> decision;
-		programa( decision, bucle );
+    bool bucle = false;
+    contactoEmail contactos[10];
+    int contadorContactos = 0; // Inicia el contador de contactos en 0
+
+    do {
+        cout << "1) Agregar un contacto" << endl;
+        cout << "2) Eliminar un contacto" << endl;
+        cout << "3) Mostrar contactos registrados" << endl;
+        cout << "4) Mostrar contactos existentes, ordenado por servidor de correo" << endl;
+        cout << "5) Salir del programa" << endl;
+        cin >> decision;
+        programa(decision, bucle, contactos, contadorContactos);
 	} while ( bucle != true );
 	
 	return 0;
